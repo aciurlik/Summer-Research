bbc784821d7929e5ee75dc0085918d5a6c440628
aciurlik <alyssa.ciurlik@furman.edu>
2017-05-23 11:46:11 -0400

    Latest Edit of Semester Panel
    Panel Constructor simplified, and SemsterPanel Drop Handler was edited.

--- SemesterPanel.java
+++ SemesterPanel.java
@@ -1,73 +1,159 @@
 
-import java.awt.Color;
-import java.awt.GridLayout;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class SemesterPanel extends JPanel {
+public class SemesterPanel extends JPanel implements ActionListener{
+
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private int buttonCounter = 0;
 	private int classCounter = 0;
 	private int requirementNumber=0;
+	private int columnNumber = 9; //This classTitle, semesterTitle, 6 classes, button
+	private int normalNumberofClasses = 4;
+	private String deleteButton = "-";
+	JPanel defaultPanel = new JPanel();
 	
-	public SemesterPanel(String classTitle, String semesterTitle){
+
+
+
+
+	public SemesterPanel(String classTitle, String semesterTitle, Color c){
+
+		//Sets up the panel that will hold one semester
 		super();
-		Schedule studentSchedule = Schedule.testSchedule();
 		this.setVisible(true);
-		this.setLayout(new GridLayout(7, 1, 5, 5));
-		this.setBackground(Color.blue);
+		defaultPanel.setLayout(new GridLayout(columnNumber, 1, 5, 5));
+		this.setBackground(c); //This allows the schedule Panel to control the color
+		defaultPanel.setBackground(this.getBackground());
+
+		//Header
 		JLabel ClassTitle = new JLabel(classTitle);
-		this.add(ClassTitle);
+		defaultPanel.add(ClassTitle);
+
 		JLabel FallSpring = new JLabel(semesterTitle, JLabel.CENTER);
-		this.add(FallSpring);
+		defaultPanel.add(FallSpring);
+
+
+	
+	
 		
+		for (int i=0; i<=normalNumberofClasses; i++){
+			
 		
-		for (int i = 0; i<4; i++){
-			// if there exists a requirement filled then add a requirement or a class? add one 
-			//WHAT about AP credit where does this go? 
-			if(studentSchedule.hasNextRequirement(requirementNumber)){
-			 //add a requirement to this list
-				String prefix = studentSchedule.getRequirement(requirementNumber);
-				requirementNumber++;
-				inSemesterRequirementPanel requirementPanel = new inSemesterRequirementPanel(prefix);
-				this.add(requirementPanel);
-				classCounter++;
-				if(classCounter == 4){
-					JLabel extraCourseSpace = new JLabel("DROP NEW COURSE HERE");
-					this.add(extraCourseSpace);
-				}
-				}
+				JLabel CourseSpace = new JLabel("DROP NEW COURSE HERE");
+				defaultPanel.add(CourseSpace);
 			
-			// if counter is at 4 then add a new spot 
-			
-		else{
-			JLabel CourseSpace = new JLabel("DROP NEW COURSE HERE");
-			this.add(CourseSpace);
 		}
 
 
-	}
+
+
+		JButton deleteSemester= new JButton(deleteButton);
+		defaultPanel.add(deleteSemester);
+		deleteSemester.addActionListener(this);
+		this.setLayout(new GridLayout(1, 1, 0, 0)); //Fits Default/Hide Panel to cover SemesterPanel. 
+		this.setVisible(true);
+		this.add(defaultPanel);
 
 	}
 
-	
-	
+
+
 	public int getClassCounter() {
 		return classCounter;
 	}
 
 
-	
+
 
 	public int getRequirementNumber() {
 		return requirementNumber;
 	}
 
+
+
+
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		buttonCounter++;
+		System.out.println(buttonCounter);
+
+		String show = "Show Semester";
+		this.remove(defaultPanel);
+		JPanel hide = new JPanel();
+		hide.setBackground(Color.pink);
+		JButton showSemester = new JButton(show);
+		showSemester.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				remove(hide);
+				add(defaultPanel);
+			}
+		}
+				);
+
+
+		//TO DO: Figure out repaint glitch
+		hide.add(showSemester);
+		showSemester.repaint(1);
+		hide.repaint(1);
+
+		this.add(hide);
+		this.repaint(1);
+
+	}
+
 	
+	private class SemesterPanelDropHandler extends PanelDropHandler{
+
+		@Override
+		public void recievedDrop(Container receiver, Component draggedItem) {
+			receiver = (SemesterPanel) receiver;
+			draggedItem = (RequirementPanel) draggedItem;
+			receiver.addReq(draggedItem.getRequirement());
+			if (receiver.semester.addScheduleElement(draggedItem.getRequirement())){
+				inSemesterRequirementPanel requirementPanel = new inSemesterRequirementPanel(draggedItem.getRequirement);
+				receiver.add(draggedItem.getRequirement.)
+
+
+
+
+
+
+			}
+
+
+		}
+
+	}
 }
-	
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
