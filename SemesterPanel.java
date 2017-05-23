bed1d45a3f5f22fa2001e461b59254d9276605d2
aciurlik <alyssa.ciurlik@furman.edu>
2017-05-22 23:25:47 -0400

    PanelsUpdates
    Refined these Panels and allowed them to take in ScheduleElement, changed the communication from SemesterPanel to Semester class instead of Schedule Panel.  This allows for greater control of communication, between the GUI and the background data.

--- SemesterPanel.java
+++ SemesterPanel.java
@@ -1,57 +1,87 @@
 
-import java.awt.Color;
-import java.awt.GridLayout;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class SemesterPanel extends JPanel {
+public class SemesterPanel extends JPanel implements ActionListener{
 
+	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private int buttonCounter = 0;
 	private int classCounter = 0;
 	private int requirementNumber=0;
+	private int columnNumber = 9; //This classTitle, semesterTitle, 6 classes, button
+	JPanel defaultPanel = new JPanel();
 	
-	public SemesterPanel(String classTitle, String semesterTitle){
+	
+	
+	
+	
+	public SemesterPanel(String classTitle, String semesterTitle, Color c){
+		//change Parameter to semesterDate and do schedule.getclass title semesterDate.getSeason()
+	
 		super();
-		Schedule studentSchedule = Schedule.testSchedule();
+
 		this.setVisible(true);
-		this.setLayout(new GridLayout(7, 1, 5, 5));
-		this.setBackground(Color.blue);
+		defaultPanel.setLayout(new GridLayout(columnNumber, 1, 5, 5));
+		this.setBackground(c);
+		defaultPanel.setBackground(this.getBackground());
+		
 		JLabel ClassTitle = new JLabel(classTitle);
-		this.add(ClassTitle);
+		defaultPanel.add(ClassTitle);
+		
 		JLabel FallSpring = new JLabel(semesterTitle, JLabel.CENTER);
-		this.add(FallSpring);
+		defaultPanel.add(FallSpring);
 		
 		
-		for (int i = 0; i<4; i++){
-			// if there exists a requirement filled then add a requirement or a class? add one 
-			//WHAT about AP credit where does this go? 
-			if(studentSchedule.hasNextRequirement(requirementNumber)){
-			 //add a requirement to this list
-				String prefix = studentSchedule.getRequirement(requirementNumber);
-				requirementNumber++;
-				inSemesterRequirementPanel requirementPanel = new inSemesterRequirementPanel(prefix);
-				this.add(requirementPanel);
-				classCounter++;
-				if(classCounter == 4){
-					JLabel extraCourseSpace = new JLabel("DROP NEW COURSE HERE");
-					this.add(extraCourseSpace);
-				}
-				}
+	
+		
+		
+	
+		Semester studentSemester = new Semester(new SemesterDate(2018, SemesterDate.FALL));
+		
+		
+		ArrayList<ScheduleElement> listOfClasses = studentSemester.getElements();
+		for (int i=0; i< listOfClasses.size(); i++){
+			ScheduleElement currentElement =(ScheduleElement) listOfClasses.get(i);
 			
-			// if counter is at 4 then add a new spot 
+			inSemesterRequirementPanel requirementPanel = new inSemesterRequirementPanel(currentElement);
+			defaultPanel.add(requirementPanel);
+			classCounter++;
+			if(classCounter == 4){
+				JLabel extraCourseSpace = new JLabel("DROP NEW COURSE HERE");
+				defaultPanel.add(extraCourseSpace);
+			}
 			
-		else{
-			JLabel CourseSpace = new JLabel("DROP NEW COURSE HERE");
-			this.add(CourseSpace);
-		}
-
-
+		
+		// if counter is at 4 then add a new spot 
+	else{
+		JLabel CourseSpace = new JLabel("DROP NEW COURSE HERE");
+		defaultPanel.add(CourseSpace);
 	}
-
+		}
+		
+		
+		
+	
+		JButton deleteSemester= new JButton("-");
+		defaultPanel.add(deleteSemester);
+		deleteSemester.addActionListener(this);
+		this.setLayout(new GridLayout(1, 1, 0, 0));
+		this.setVisible(true);
+		this.add(defaultPanel);
+		
 	}
 
 	
@@ -67,7 +97,81 @@ public class SemesterPanel extends JPanel {
 		return requirementNumber;
 	}
 
+
+
+
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		buttonCounter++;
+		System.out.println(buttonCounter);
+
+		String show = "Show Semester";
+		this.remove(defaultPanel);
+		JPanel hide = new JPanel();
+		hide.setBackground(Color.pink);
+		JButton showSemester = new JButton(show);
+		showSemester.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				remove(hide);
+				add(defaultPanel);
+				}
+			}
+);
+		
 	
+	
+		hide.add(showSemester);
+		showSemester.repaint(1);
+		hide.repaint(1);
+
+		this.add(hide);
+		this.repaint(1);
+		
 }
+	
+	/**
+	 *  private class SemesterPanelDropHandler extends PanelDropHandler{
+
+				@Override
+				public void recievedDrop(Container receiver, Component draggedItem) {
+					receiver = (SemesterPanel) receiver;
+					draggedItem = (RequirementPanel) draggedItem;
+					reciever.addReq(draggedItem.getRequirement());
+					reciever.semester.addScheduleElement(draggedItem.getRequirement());
+					
+					Update GUI 
+					
+					
+					
+					
+				
+						  		}
+					
+					
+				}
+	 
+				}
+	 * 
+	 *
+	 */
+
+	 
+}
+	
+
+
+
+	
+	
+
+
+	
+
+
+
 	
  
