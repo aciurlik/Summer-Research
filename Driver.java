3d476756dea5cab73a4822042611f431e9dc21f7
aciurlik <alyssa.ciurlik@furman.edu>
2017-05-24 14:27:39 -0400

    Alter DropDown and Driver

--- Driver.java
+++ Driver.java
@@ -7,21 +7,57 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Driver {
-	public CourseList masterList;
+	public static  CourseList masterList;
 	
 	public Driver(){
 		CourseList masterList = new CourseList();
 		this.masterList = masterList;
 		int[] meetingDays = {Time.MONDAY, Time.WEDNESDAY, Time.FRIDAY};
 		Course a = new Course(new Prefix("MTH", 220), new SemesterDate(2017, SemesterDate.FALL), "Fray", meetingDays, 4, 02);
+		Course b = new Course(new Prefix("MTH", 220), new SemesterDate(2017, SemesterDate.FALL), "Harris", meetingDays, 4, 02);
 		masterList.add(a);
+		masterList.add(b);
 	}
-
+}
 	
-	public static void main(String[] args){
-	
-		
-		
+	/**public static void main(String[] args){
+	 *    JFrame frame = new JFrame();
+	  
+	   //frame.setSize(2000, 2000);
+	   
+	   
+	   
+	   JPanel p = new JPanel();
+	//   p.setVisible(true);
+	   
+	   Schedule test = Schedule.testSchedule();
+	   
+	   
+	   SchedulePanel s = new SchedulePanel(test);
+	   
+	  
+	  // s.setVisible(true);
+	   
+	  
+	   p.add(s);
+	  
+	  Requirement req = Requirement.testRequirement();
+	  RequirementPanel tester= new RequirementPanel(req);
+	 // tester.setVisible(true);
+	  p.add(tester);
+	  
+	  RequirementListPanel reqs = new RequirementListPanel(test);
+	   reqs.add(tester);
+	   reqs.add(tester);
+	  // reqs.setVisible(true);
+	   p.add(reqs);
+	  
+	   frame.add(p);
+	   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	   frame.pack();
+	   frame.setVisible(true);
+	   
+	   	
 	   JFrame frame = new JFrame();
 	  
 	   //frame.setSize(2000, 2000);
@@ -65,3 +101,15 @@ public class Driver {
 
 }
 }
+	    
+	   
+	  
+	  
+	   
+
+}
+}
+	 */
+	
+		
+	
--- ScheduleElementPanel.java
+++ ScheduleElementPanel.java
@@ -9,9 +9,14 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+
+
 public class ScheduleElementPanel extends JPanel {
+	private int updateCount = 0;
 	private Requirement r;
 	private ScheduleElement s;
+//	Driver coursesSatisfy = new Driver();
+	JComboBox  requirmentDropDown = new JComboBox();
 	
 	public ScheduleElementPanel(ScheduleElement s) { 
 		super();
@@ -33,24 +38,28 @@ public class ScheduleElementPanel extends JPanel {
 		public void updatePanel(){ //This can be taken out later
 			JLabel requirementLabel = new JLabel(s.getDisplayString());
 			this.add(requirementLabel);
-			if(s instanceof Requirement){
+			if(s instanceof Requirement) {
 				dropDownRequirment();
 			}
+			}
 			//If course is dropped then no dropDown Panel is needed
+			
 		
-		}
+		
 		
 		public void dropDownRequirment(){
-			JComboBox  requirmentDropDown = new JComboBox();
-			Driver coursesSatisfy = new Driver();
-			ArrayList listOfCourses = coursesSatisfy.masterList.getCoursesSatisfying(this.r);
+			
+			ArrayList<Course> listOfCourses = Driver.masterList.getCoursesSatisfying(this.r);
 			for( int i = 0; i< listOfCourses.size(); i++){
 				requirmentDropDown.addItem(listOfCourses.get(i));
-				
 			}
 			this.add(requirmentDropDown);
+			System.out.println("The drop is added");
 			
 		}
+		
+		
+		
 }
 		
  		
