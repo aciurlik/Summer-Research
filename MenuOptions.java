3a101949a9647ef04cc88132b1563987ff33c599
aciurlik <alyssa.ciurlik@furman.edu>
2017-05-31 09:55:49 -0400

    Add MenuOptions, and add Internship Link

---
+++ MenuOptions.java
@@ -0,0 +1,13 @@
+
+public class MenuOptions {
+	public static final  String addMajor = "Add Major";
+	public static final  String addMinor = "Add Minor";
+	public static final  String addTrack = "Add Track";
+	public static final  String addInternship = "Explore Internships";
+	public static final  String addResearch = "Add Research";
+	public static final  String addStudyAway = "Add Study Away";
+	public static final  String addMayX = "Add MayX";
+	public static final  String addSummerClass = "Add Summer Class";
+	
+
+}
